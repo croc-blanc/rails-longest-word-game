884d164f20fd0d3d0a2d89949d0d3256c97a11ec
gregoire dupouy <gregoire.dupouy@gmail.com>
2017-08-09 16:06:25 +0200

    pb 1 from CRUD ok

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  get 'pages/game'
+  get 'game', to: 'pages#game', as: 'game'
+
+  get 'pages/score'
+  get 'score', to: 'pages#score', as: 'score'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
